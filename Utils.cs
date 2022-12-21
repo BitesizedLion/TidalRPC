90c2d7bbf57d09e972618b147874cf367d6dddea
Casper S <casper@caspers.club>
2022-12-21 23:20:58 +0100

    Moved away from `dynamic` to strongly-typed class for `GetAddress()`

--- Utils.cs
+++ Utils.cs
@@ -5,14 +5,15 @@ using System.Text;
 
 namespace TidalRPC
 {
-    public static class Utils
+    public class Utils
     {
+        private static readonly string programDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+        private static readonly string corePropsPath = Path.Combine(programDataPath, "SteelSeries/SteelSeries Engine 3/coreProps.json");
+
         // Method for checking if the coreProps.json file exists, if it doesn't, creates it.
         public static void CheckCoreProps()
         {
-            string programDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-            string filePath = Path.Combine(programDataPath, "SteelSeries/SteelSeries Engine 3/coreProps.json");
-            string directoryPath = Path.GetDirectoryName(filePath);
+            string directoryPath = Path.GetDirectoryName(corePropsPath);
 
             if (!Directory.Exists(directoryPath))
             {
@@ -20,29 +21,28 @@ namespace TidalRPC
                 Directory.CreateDirectory(directoryPath);
             }
 
-            if (!File.Exists(filePath))
+            if (!File.Exists(corePropsPath))
             {
                 Console.WriteLine("CheckCoreProps: coreProps does not exist, creating it");
+                
                 string contents = "{\"address\":\"127.0.0.1:3650\"}";
-                File.WriteAllText(filePath, contents);
+
+                File.WriteAllText(corePropsPath, contents);
             } else { Console.WriteLine("CheckCoreProps: coreProps was found"); }
         }
 
         // Method for getting the address
         public static string GetAddress()
         {
-            string programDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-            string filePath = Path.Combine(programDataPath, "SteelSeries/SteelSeries Engine 3/coreProps.json");
-
-            if (!File.Exists(filePath))
+            if (!File.Exists(corePropsPath))
             {
                 Console.WriteLine("GetAddress: coreProps does not exist, returning default address");
                 return "http://127.0.0.1:3650/";
             }
 
-            string jsonString = File.ReadAllText(filePath);
-            dynamic json = JsonConvert.DeserializeObject(jsonString);
-            string address = $"http://{json["address"]}/";
+            string jsonString = File.ReadAllText(corePropsPath);
+            CoreProps json = JsonConvert.DeserializeObject<CoreProps>(jsonString);
+            string address = $"http://{json.Address}/";
 
             return address;
         }
@@ -69,6 +69,12 @@ namespace TidalRPC
             {
                 originalOut.Write(string.Format("[{0}] {1}", DateTime.Now, message));
             }
+        }
+
+        // Class for coreProps.json
+        public class CoreProps
+        {
+            public string Address { get; set; }
         }
     }
 }
