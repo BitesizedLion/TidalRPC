fffa102cd661f289c8af1ac6e704d2fa728dedba
Casper S <casper@caspers.club>
2023-08-21 00:38:55 +0200

    Add country code utility with validation and user prompt

--- Utils.cs
+++ Utils.cs
@@ -1,7 +1,11 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Globalization;
+using System.Linq;
 using System.IO;
 using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace TidalRPC
 {
@@ -75,6 +79,96 @@ namespace TidalRPC
         public class CoreProps
         {
             public string Address { get; set; }
+        }
+
+        public static class CountryCodeUtility
+        {
+            public static bool IsCountryCodeValid(string countryCode)
+            {
+                return CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                                  .Select(culture => new RegionInfo(culture.LCID))
+                                  .Any(region => region.TwoLetterISORegionName.Equals(countryCode, StringComparison.OrdinalIgnoreCase));
+            }
+
+            public static string PromptForValidCountryCode()
+            {
+                while (true)
+                {
+                    string userInput = PromptInputBox("Please enter a two-character ISO country code:", "Enter Country Code", "");
+
+                    if (IsCountryCodeValid(userInput))
+                    {
+                        return SetCountryCode(userInput);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid country code. Please enter a valid two-character ISO country code.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+
+            // Yes, it's awful but I'm not fixing it. "If it works, don't touch it"
+            private static string PromptInputBox(string prompt, string title, string defaultValue)
+            {
+                Form promptForm = new Form()
+                {
+                    Width = 300,
+                    Height = 150,
+                    FormBorderStyle = FormBorderStyle.FixedDialog,
+                    Text = title,
+                    StartPosition = FormStartPosition.CenterScreen
+                };
+
+                Label label = new Label() { Left = 20, Top = 20, Width = 260, Text = prompt };
+                TextBox textBox = new TextBox() { Left = 20, Top = 50, Width = 260, Text = defaultValue };
+                Button confirmation = new Button() { Text = "Ok", Left = 150, Width = 60, Top = 80, DialogResult = DialogResult.OK };
+                Button cancel = new Button() { Text = "Cancel", Left = 220, Width = 60, Top = 80, DialogResult = DialogResult.Cancel };
+
+                confirmation.Click += (sender, e) => promptForm.Close();
+                cancel.Click += (sender, e) => promptForm.Close();
+
+                promptForm.Controls.Add(textBox);
+                promptForm.Controls.Add(confirmation);
+                promptForm.Controls.Add(cancel);
+                promptForm.Controls.Add(label);
+
+                promptForm.AcceptButton = confirmation;
+                promptForm.CancelButton = cancel;
+
+                return promptForm.ShowDialog() == DialogResult.OK ? textBox.Text : "US";
+            }
+
+            // Method for checking if update checking is enabled
+            private static string GetCountryCode()
+            {
+                string keyName = "SOFTWARE\\TidalRPC\\Settings";
+                string valueName = "CountryCode";
+
+                using RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName);
+
+                if (key == null)
+                {
+                    SetCountryCode("US"); // default to US
+                    return "US";
+                }
+
+                return (string)key.GetValue(valueName, "US");
+            }
+
+            // Method for setting the country code
+            private static string SetCountryCode(string countryCode)
+            {
+                string keyName = "SOFTWARE\\TidalRPC\\Settings";
+                string valueName = "CountryCode";
+
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName, true);
+
+                if (key == null) key = Registry.CurrentUser.CreateSubKey(keyName); // Create the registry key if it does not exist
+
+                key.SetValue(valueName, countryCode, RegistryValueKind.String);
+
+                return countryCode; // Return the countryCode because testing, TODO: REMOVE
+            }
         }
     }
 }
